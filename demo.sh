eb331dd36e443ac6f067e76538b4b41192feeaea
gregbolet <gbolet@vt.edu>
2021-12-05 18:45:06 -0500

    improved demo code

--- demo.sh
+++ demo.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-make
+SAMPLES_PER_SEC=5
+SAMPLING_TIME_IN_SECS=30
+OUTPUT_FILE=testout.csv
 
 echo "Starting Demo! Writing to file: ${OUTPUT_FILE}"
 
 # Start taking samples
-./main 5 30 > ${OUTPUT_FILE} &
+./main $SAMPLES_PER_SEC $SAMPLING_TIME_IN_SECS > ${OUTPUT_FILE} &
+
+waitpid=$!
 
 # Wait 10 seconds and run the benchmark
 sleep 10
 ./NPB/SNU_NPB_2019/NPB3.3-OMP-C/bin/ft.B.x
 
+echo "FT Benchmark Complete!"
+echo "Waiting for sampling to finish..."
+
 # Wait for sampling to finish
+wait $waitpid
 
 echo "Demo complete!"
-
